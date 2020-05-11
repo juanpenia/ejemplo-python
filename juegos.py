18a26c36ace88d15bead2bf0391cf0e77217fc07
Juan Peña <juanpea_98@live.com.ar>
2020-05-11 18:21:09 -0300

    bug fix

--- juegos.py
+++ juegos.py
@@ -6,54 +6,57 @@ import PySimpleGUI as sg
 from os.path import isfile
 
 def FileName():
-	return "historial.json"
+    return "historial.json"
+    
+JuegosValidos = ('Ahorcado', 'TA-TE-TI', 'Otello')
 
 def GuardarAJSON(nombre, juego):
-	dato_nuevo = "Nombre: {}. Juego: {}".format(nombre, juego)
-	if(isfile(FileName())):
-		with open(FileName()) as arc:
-			datos = json.load(arc)
-			datos.append(dato_nuevo)
+    dato_nuevo = "Nombre: {}. Juego: {}".format(nombre, juego)
+    if(isfile(FileName())):
+        with open(FileName()) as arc:
+            datos = json.load(arc)
+            datos.append(dato_nuevo)
 	
-	else:
-		datos = [dato_nuevo]
+    else:
+        datos = [dato_nuevo]
 
-	with open(FileName(), "w") as arc:
-		json.dump(datos, arc, indent=4)
+    with open(FileName(), "w") as arc:
+        json.dump(datos, arc, indent=4)
 
 def interfaz():
 
-	sg.theme("DarkAmber")
+    sg.theme("DarkAmber")
 
-	layout =[
-			[sg.Text('Ingrese nombre del jugador: '), sg.InputText(size=(8, 0))],
-			[sg.Text('Selecciona el juego:'), sg.Combo(values=('Ahorcado', 'TA-TE-TI', 'Otello'), size=(10, 10), key='lista_juegos')],
-			[sg.Submit(), sg.Cancel()] ]
+    layout =[
+            [sg.Text('Ingrese nombre del jugador: '), sg.InputText(size=(8, 0))],
+            [sg.Text('Selecciona el juego:'), sg.Combo(values=JuegosValidos, size=(10, 10), readonly=True, key='lista_juegos')],
+            [sg.Submit(), sg.Cancel()] ]
          
 
-	window = sg.Window('Juegos', layout, size=(300, 120))
+    window = sg.Window('Juegos', layout, size=(300, 120))
+    
+    while True:
+        event, values = window.read()
+        if event is 'Submit':
 
-	while True:
-		event, values = window.read()
-		if event in (None, 'Submit'):
+            juego = values['lista_juegos'] # valor seleccionado del combo
+            if juego in JuegosValidos:
+                GuardarAJSON(values[0], juego)
+                if juego == 'Ahorcado':
+                    hangman.main()
+                elif juego == 'TA-TE-TI':
+                    tictactoeModificado.main()
+                elif juego == 'Otello':
+                    reversegam.main()
 
-			juego = values['lista_juegos'] # valor seleccionado del combo
-			GuardarAJSON(values[0], juego)
-			if juego == 'Ahorcado':
-				hangman.main()
-			elif juego == 'TA-TE-TI':
-				tictactoeModificado.main()
-			elif juego == 'Otello':
-				reversegam.main()
-
-		if event in (None, 'Cancel'):
-			break
-	window.close()
-
+        if event in (None, 'Cancel'):
+            break
+            
+    window.close()
 
 
 def main(args):
-	interfaz()
+    interfaz()
 	
 if __name__ == '__main__':
 	import sys
